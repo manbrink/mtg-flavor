01a28da4313f48b514076ffb1f7c75c69ae3c6d9
manbrink <mbrinkma13@gmail.com>
2023-05-22 14:40:51 -0700

    main card, shuffle responsiveness

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,18 +14,24 @@ export const useDB = routeLoader$(async (requestEv) => {
 
   const { data } = await supabaseClient.rpc("shuffle");
 
-  return { data };
+  const mainCardData = await supabaseClient
+    .from("cards")
+    .select()
+    .eq("id", "6dca045d-a0f4-444e-b44a-388930b74da5");
+
+  return { data, mainCardData };
 });
 
 export default component$(() => {
   const cardSignal = useDB();
   const cardData = cardSignal.value.data;
+  const mainCardData = cardSignal.value.mainCardData.data[0];
 
   return (
     <>
       <header class="pb-2 pt-2">
         <h1 class="text-center text-2xl font-bold">
-          Magic the Gathering Flavor Text
+          Magic the Gathering Flavor
         </h1>
         <div class="flex justify-center pb-2 pt-4">
           <p class="p-2 text-center underline">
@@ -49,21 +55,21 @@ export default component$(() => {
       <section class="mx-auto flex flex-col items-center justify-center">
         <div class="shadow-lg">
           <div class="pb-1 text-sm opacity-70">
-            upvotes: {cardData.up_votes}
+            upvotes: {mainCardData.up_votes}
           </div>
           <div class="w-[400px] overflow-hidden">
             <Image
-              src={cardData.scryfall_art_crop_url}
+              src={mainCardData.scryfall_art_crop_url}
               layout="fullWidth"
-              alt={cardData.name}
+              alt={mainCardData.name}
             />
           </div>
         </div>
 
         <div class="p-4">
-          <h1 class="pb-1 text-2xl">{cardData.name}</h1>
-          <h2 class="pb-1 text-lg opacity-70">{cardData.set_name}</h2>
-          <p class="text-base italic opacity-70">{cardData.flavor_text}</p>
+          <h1 class="pb-1 text-2xl">{mainCardData.name}</h1>
+          <h2 class="pb-1 text-lg opacity-70">{mainCardData.set_name}</h2>
+          <p class="text-base italic opacity-70">{mainCardData.flavor_text}</p>
         </div>
       </section>
 
@@ -73,15 +79,15 @@ export default component$(() => {
           trademarks of Wizards of the Coast LLC in the United States and other
           countries. © 1993-2023 Wizards. All Rights Reserved.
           <br />
-          XXX is not affiliated with, endorsed, sponsored, or specifically
-          approved by Wizards of the Coast LLC. XXX may use the trademarks and
-          other intellectual property of Wizards of the Coast LLC, which is
-          permitted under Wizards&apos; Fan Content Policy. MAGIC: THE
-          GATHERING® is a trademark of Wizards of the Coast. For more
+          MtG Flavor is not affiliated with, endorsed, sponsored, or
+          specifically approved by Wizards of the Coast LLC. MtG Flavor may use
+          the trademarks and other intellectual property of Wizards of the Coast
+          LLC, which is permitted under Wizards&apos; Fan Content Policy. MAGIC:
+          THE GATHERING® is a trademark of Wizards of the Coast. For more
           information about Wizards of the Coast or any of Wizards&apos;
           trademarks or other intellectual property, please visit their website
           at https://company.wizards.com/.
-          <br />© 2023 XXX - Terms of Service - Privacy Policy
+          <br />© 2023 MtG Flavor - Terms of Service - Privacy Policy
         </p>
       </footer>
     </>
--- src/routes/shuffle/[cardId]/index.tsx
+++ src/routes/shuffle/[cardId]/index.tsx
@@ -64,47 +64,52 @@ export default component$(() => {
         </a>
       </div>
 
-      <header class="pb-4 pt-2">
-        <div>
-          <Form class="flex justify-center">
-            <a class="px-3 pb-2 text-2xl" href={`/shuffle/${shuffleData.id}`}>
-              <LuRefreshCcw />
-            </a>
-            <div class="cursor-pointer px-3 pb-2 text-2xl">
-              <LuThumbsUp
-                preventdefault:click
-                onClick$={async () => {
-                  await action.submit({
-                    id: cardData.id,
-                    up_votes: cardData.up_votes + 1,
-                  });
-                }}
+      <div class="flex flex-col">
+        <header class="order-last pb-4 pt-4 md:order-first md:pt-2">
+          <div>
+            <Form class="flex justify-center">
+              <a
+                class="px-5 pb-2 text-5xl md:px-3 md:text-2xl"
+                href={`/shuffle/${shuffleData.id}`}
+              >
+                <LuRefreshCcw />
+              </a>
+              <div class="cursor-pointer px-5 pb-2 text-5xl md:px-3 md:text-2xl">
+                <LuThumbsUp
+                  preventdefault:click
+                  onClick$={async () => {
+                    await action.submit({
+                      id: cardData.id,
+                      up_votes: cardData.up_votes + 1,
+                    });
+                  }}
+                />
+              </div>
+            </Form>
+          </div>
+        </header>
+
+        <section class="mx-auto flex flex-col items-center justify-center pt-10 md:pt-0">
+          <div class="shadow-lg">
+            <div class="pb-1 text-sm opacity-70">
+              upvotes: {cardData.up_votes}
+            </div>
+            <div class="overflow-hidden">
+              <Image
+                src={cardData.scryfall_art_crop_url}
+                layout="fullWidth"
+                alt={cardData.name}
               />
             </div>
-          </Form>
-        </div>
-      </header>
-
-      <section class="mx-auto flex flex-col items-center justify-center">
-        <div class="shadow-lg">
-          <div class="pb-1 text-sm opacity-70">
-            upvotes: {cardData.up_votes}
           </div>
-          <div class="overflow-hidden">
-            <Image
-              src={cardData.scryfall_art_crop_url}
-              layout="fullWidth"
-              alt={cardData.name}
-            />
-          </div>
-        </div>
 
-        <div class="p-4">
-          <h1 class="pb-1 text-2xl">{cardData.name}</h1>
-          <h2 class="pb-1 text-lg opacity-70">{cardData.set_name}</h2>
-          <p class="text-base italic opacity-70">{cardData.flavor_text}</p>
-        </div>
-      </section>
+          <div class="p-4">
+            <h1 class="pb-1 text-2xl">{cardData.name}</h1>
+            <h2 class="pb-1 text-lg opacity-70">{cardData.set_name}</h2>
+            <p class="text-base italic opacity-70">{cardData.flavor_text}</p>
+          </div>
+        </section>
+      </div>
     </>
   );
 });
