36447261395c740014195b7bd027916c03ae2346
manbrink <mbrinkma13@gmail.com>
2023-05-23 00:14:56 -0700

    shuffle page try to avoid layout shift

--- src/routes/shuffle/[cardId]/index.tsx
+++ src/routes/shuffle/[cardId]/index.tsx
@@ -72,53 +72,53 @@ export default component$(() => {
         </a>
       </div>
 
-      <div class="flex flex-col">
-        <header class="order-last pb-4 pt-4 md:order-first md:pt-2">
-          <div>
-            <Form class="flex justify-center">
-              <a
-                class="px-5 pb-2 text-5xl md:px-3 md:text-2xl"
-                href={`/shuffle/${shuffleData.id}`}
-              >
-                <LuRefreshCcw />
-              </a>
-              <div class="cursor-pointer px-5 pb-2 text-5xl md:px-3 md:text-2xl">
-                <LuThumbsUp
-                  onClick$={async () => {
-                    if (!inLocalStorage(cardData)) {
-                      localStorage.setItem(cardData.id, "true");
-                      await action.submit({
-                        id: cardData.id,
-                        up_votes: cardData.up_votes + 1,
-                      });
-                    } else {
-                      alert("You already upvoted this card!");
-                    }
-                  }}
-                />
-              </div>
-            </Form>
-          </div>
-        </header>
+      <div class="w-full pb-2 pt-20 md:pt-10">
+        <div>
+          <Form class="flex justify-center">
+            <a class="px-5 pb-2 text-5xl" href={`/shuffle/${shuffleData.id}`}>
+              <LuRefreshCcw />
+            </a>
+            <div class="cursor-pointer px-5 pb-2 text-5xl">
+              <LuThumbsUp
+                onClick$={async () => {
+                  if (!inLocalStorage(cardData)) {
+                    localStorage.setItem(cardData.id, "true");
+                    await action.submit({
+                      id: cardData.id,
+                      up_votes: cardData.up_votes + 1,
+                    });
+                  } else {
+                    alert("You already upvoted this card!");
+                  }
+                }}
+              />
+            </div>
+          </Form>
+        </div>
+      </div>
 
-        <section class="mx-auto flex flex-col items-center justify-center pt-10 md:pt-0">
+      <div class="flex flex-col">
+        <section class="mx-auto flex flex-col items-center justify-center">
+          <div class="px-4 pb-6 pt-4">
+            <h1 class="pb-1 text-2xl">{cardData.name}</h1>
+            <h2 class="pb-1 text-lg opacity-70">{cardData.set_name}</h2>
+            <p class="text-base italic opacity-70">{cardData.flavor_text}</p>
+          </div>
+
           <div class="shadow-lg">
             <div class="pb-1 text-sm opacity-70">
               upvotes: {cardData.up_votes}
             </div>
+
             <div class="overflow-hidden">
               <Image
                 src={cardData.scryfall_art_crop_url}
-                layout="fullWidth"
+                layout="fixed"
+                width={450}
+                height={600}
                 alt={cardData.name}
               />
             </div>
-          </div>
-
-          <div class="p-4">
-            <h1 class="pb-1 text-2xl">{cardData.name}</h1>
-            <h2 class="pb-1 text-lg opacity-70">{cardData.set_name}</h2>
-            <p class="text-base italic opacity-70">{cardData.flavor_text}</p>
           </div>
         </section>
       </div>
