a06537abdc2db53f7b43ebb18aacf953ee37d518
manbrink <mbrinkma13@gmail.com>
2023-05-23 21:54:32 -0700

    vercel analytics

--- src/root.tsx
+++ src/root.tsx
@@ -1,8 +1,14 @@
-import { component$ } from '@builder.io/qwik';
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
+import { component$ } from "@builder.io/qwik";
+import {
+  QwikCityProvider,
+  RouterOutlet,
+  ServiceWorkerRegister,
+} from "@builder.io/qwik-city";
+import { RouterHead } from "./components/router-head/router-head";
 
-import './global.css';
+import { inject } from "@vercel/analytics";
+
+import "./global.css";
 
 export default component$(() => {
   /**
@@ -11,6 +17,8 @@ export default component$(() => {
    *
    * Dont remove the `<head>` and `<body>` elements.
    */
+
+  inject();
 
   return (
     <QwikCityProvider>
